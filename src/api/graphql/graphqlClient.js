6b6aed4d1137a7e664e843e43f92839c73326fd2
RedFoxFinn <laborantti.antti@gmail.com>
2026-02-28 20:23:33 +0200

    conf edit

--- src/api/graphql/graphqlClient.js
+++ src/api/graphql/graphqlClient.js
@@ -5,7 +5,7 @@ import {
   HttpLink
 } from '@apollo/client'
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL || process.env.VITE_API_URL
 
 console.log('[Apollo Client] Using API URL:', API_URL)
 
