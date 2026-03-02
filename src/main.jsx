4026eabd1046592be65d29cd78b735cedcd712b3
RedFoxFinn <laborantti.antti@gmail.com>
2026-03-02 18:07:06 +0200

    improved page refreshing behaviour, v3

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/client/react'
 import { graphqlClient } from './api/graphql/graphqlClient.js'
 
@@ -9,9 +9,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ApolloProvider client={graphqlClient}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ApolloProvider>
   </StrictMode>,
 )
