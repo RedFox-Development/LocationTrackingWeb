6984fbeb38d5fe49426ba27f1431c7aa30d599a9
RedFoxFinn <laborantti.antti@gmail.com>
2026-03-02 19:31:01 +0200

    favicons set

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,28 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/LocationTrackingWeb/' : '/',
+  manifest: {
+    name: 'Location Tracking',
+    short_name: 'LocTrac',
+    description: 'A location tracking web application',
+    icons: [
+      {
+        src: './public/images/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: './public/images/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any'
+      },
+      {
+        src: './public/images/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable'
+      },
+    ],
+  },
 })
